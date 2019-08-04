4dd4904be35be2ee97f29759128959a0533d42a5
bobmatyas <ballium@protonmail.com>
2019-08-03 20:09:30 -0500

    Updates

---
+++ precache-manifest.bfa82d93bcb4becee43afcc4db630260.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80fab3dd973b55b48c98e91b91609374",
+    "url": "/react-dad-jokes/index.html"
+  },
+  {
+    "revision": "e307bdb697002ff34c7f",
+    "url": "/react-dad-jokes/static/css/main.e9a7ea4f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-dad-jokes/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e307bdb697002ff34c7f",
+    "url": "/react-dad-jokes/static/js/main.a676873f.chunk.js"
+  },
+  {
+    "revision": "860a967136e351ef7c01",
+    "url": "/react-dad-jokes/static/js/runtime~main.a58e043a.js"
+  }
+]);
